5f57d072187e45af207a578fbbd9362f4aec7b9b
Mostafa <123456>
2022-04-04 09:28:32 +0430

    2022/04/04 bug fixed

--- SmartFileBrowser/src/main/java/ir/smartdevelopers/smartfilebrowser/customClasses/GalleryLayoutManager.java
+++ SmartFileBrowser/src/main/java/ir/smartdevelopers/smartfilebrowser/customClasses/GalleryLayoutManager.java
@@ -2,8 +2,11 @@ package ir.smartdevelopers.smartfilebrowser.customClasses;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearSmoothScroller;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class GalleryLayoutManager extends GridLayoutManager {
     public GalleryLayoutManager(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
@@ -21,5 +24,35 @@ public class GalleryLayoutManager extends GridLayoutManager {
     @Override
     public boolean supportsPredictiveItemAnimations() {
         return false;
+    }
+
+    @Override
+    public void smoothScrollToPosition(RecyclerView recyclerView, RecyclerView.State state, int position) {
+
+        startSmoothScroll(new Scroller(recyclerView.getContext(),position));
+    }
+
+    public static class Scroller extends LinearSmoothScroller {
+
+        public Scroller(Context context,int targetPosition) {
+            super(context);
+            setTargetPosition(targetPosition);
+
+        }
+
+
+
+        @Override
+        protected int calculateTimeForScrolling(int dx) {
+            return 800;
+        }
+
+        @Override
+        protected int calculateTimeForDeceleration(int dx) {
+            return 200;
+        }
+
+
+
     }
 }
--- SmartFileBrowser/src/main/java/ir/smartdevelopers/smartfilebrowser/customClasses/SquareLayout.java
+++ SmartFileBrowser/src/main/java/ir/smartdevelopers/smartfilebrowser/customClasses/SquareLayout.java
@@ -3,32 +3,40 @@ package ir.smartdevelopers.smartfilebrowser.customClasses;
 import android.content.Context;
 import android.os.Build;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 
+import ir.smartdevelopers.smartfilebrowser.R;
+
 public class SquareLayout extends FrameLayout {
+    private int size=0;
+
     public SquareLayout(@NonNull Context context) {
-        super(context);
+        this(context,null);
     }
 
-    public SquareLayout(@NonNull Context context, @Nullable @org.jetbrains.annotations.Nullable AttributeSet attrs) {
-        super(context, attrs);
+    public SquareLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs,0);
     }
 
     public SquareLayout(@NonNull Context context, @Nullable @org.jetbrains.annotations.Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        DisplayMetrics metrics=getResources().getDisplayMetrics();
+        int deviceWidth=metrics.widthPixels;
+        int gapSize=getResources().getDimensionPixelSize(R.dimen.sfb_gallery_gap_size);
+        int imageCount=getResources().getInteger(R.integer.sfb_gallery_grid);
+        int gapCount=imageCount+1;
+        size=(deviceWidth-(gapSize*gapCount))/imageCount;
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    public SquareLayout(@NonNull Context context, @Nullable @org.jetbrains.annotations.Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
-    }
+
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+        super.onMeasure(MeasureSpec.makeMeasureSpec(size,MeasureSpec.EXACTLY), MeasureSpec.makeMeasureSpec(size,MeasureSpec.EXACTLY));
     }
 }
